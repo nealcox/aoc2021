a6463f419a8eb162c7a891a609bf6c004f3f7d2e
sax <26412936+nealcox@users.noreply.github.com>
2021-12-15 08:12:15 +0000

    day 15

---
+++ day15/part2.py
@@ -0,0 +1,80 @@
+import sys
+from collections import defaultdict
+
+
+def main():
+    filename = "input.txt"
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+    with open(filename) as f:
+        input_text = f.read().strip()
+    print(f"Answer: {calculate(input_text)}")
+
+
+def calculate(input_text):
+
+    answer = 0
+    area, height, width = get_map(input_text)
+    best = defaultdict(lambda: float("inf"))
+    big_area = defaultdict(lambda: float("inf"))
+
+    for r in range(5 * height):
+        for c in range(5 * width):
+            big_area[r, c] = get_big_area(r, c, area, height, width)
+
+    best[0, 0] = 0
+    todo = neighbours(0, 0)
+    while todo:
+        r, c = todo.pop(0)
+        possible = big_area[r, c] + min(best[p] for p in neighbours(r, c))
+        if possible < best[r, c]:
+            best[r, c] = possible
+            todo.extend(neighbours(r, c))
+
+    return best[height * 5 - 1, width * 5 - 1] - best[0, 0]
+
+
+def neighbours(r, c):
+    return [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]
+
+
+def get_big_area(r, c, area, height, width):
+    return (area[r % height, c % width] + r // height + c // width - 1) % 9 + 1
+
+
+def get_map(s):
+    area = defaultdict(lambda: float("inf"))
+
+    lines = s.splitlines()
+    height = len(lines)
+    width = len(lines[0])
+
+    for r, row in enumerate(lines):
+        for c, char in enumerate(row):
+            area[r, c] = int(char)
+    given = (area, height, width)
+    return given
+
+
+example = """\
+1163751742
+1381373672
+2136511328
+3694931569
+7463417111
+1319128137
+1359912421
+3125421639
+1293138521
+2311944581
+"""
+
+example_answer = 315
+
+
+def test_example():
+    assert calculate(example) == example_answer
+
+
+if __name__ == "__main__":
+    exit(main())
